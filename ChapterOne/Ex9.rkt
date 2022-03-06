2ca6c138ff13d460c246d6e35275aa8c62de1468
Astoria <70237446+mwastoria@users.noreply.github.com>
2022-03-06 19:47:32 -0300

    Update Ex9.rkt

--- ChapterOne/Ex9.rkt
+++ ChapterOne/Ex9.rkt
@@ -1,6 +1,10 @@
+;Library for the image type.
 (require 2htdp/image)
+
 ; Definition given by the exercise. Substitute the "..." template for a primitive of your desired type so as to test the xpression.
 (define in ...)
+
+; Expression.
 (cond [(string? in) (string-length in)]
       [(image? in) (* (image-width in) (image-height in))]
       [(number? in) (abs in)]
