cf0876aacf2150eff4723205db6fb78a42cb50cd
Astoria <70237446+mwastoria@users.noreply.github.com>
2022-03-06 12:47:25 -0300

    Update Ex8.rkt

--- ChapterOne/Ex8.rkt
+++ ChapterOne/Ex8.rkt
@@ -3,7 +3,13 @@
 (define cat .)
 (define rect (rectangle 50 10 "solid" "blue"))
 
-;Desired Expression. Substitute cat for rect to check whether rect is tall or wide.
+;Desired Expression. Substitute cat for rect to check whether rect is tall or wide. Do the same for the "tall-wide-square" function.
 (define tall-or-wide
   (if
    (>= (image-height cat) (image-width cat)) "tall" "wide"))
+
+(define tall-wide-square
+  (cond
+   [(> (image-height cat) (image-width cat)) "tall"]
+   [(= (image-height cat) (image-width cat)) "square"]
+   [(< (image-height cat) (image-width cat)) "wide"]))
