543a291135b5320663085a6c17be4e0e1c93d1f5
Astoria <70237446+mwastoria@users.noreply.github.com>
2022-03-06 12:38:33 -0300

    Update Ex9.rkt

--- ChapterOne/Ex9.rkt
+++ ChapterOne/Ex9.rkt
@@ -2,8 +2,9 @@
 ; Definition given by the exercise. Substitute the dot for the cat image.
 (define cat .)
 (define rect (rectangle 50 10 "solid" "blue"))
-
-;Desired Expression. Substitute cat for rect to check whether rect is tall or wide. Do the same for the "tall-wide-square" function.
+(define sqr (square 
+;Desired Expression. Substitute cat for rect sqr to check whether rect or sqr are tall or wide. Do the same for the "tall-wide-square" function, except it can check
+if an image is square or not too.
 (define tall-or-wide
   (if
    (>= (image-height cat) (image-width cat)) "tall" "wide"))
