2b23eab4d3fda3c99babab8906e0a436a65c6072
Astoria <70237446+mwastoria@users.noreply.github.com>
2022-03-06 10:03:43 -0300

    Create ex6.rkt

---
+++ ChapterOne/ex6.rkt
@@ -0,0 +1,6 @@
+(require 2htdp/image)
+; Definition given by the exercise. Substitute the dot by the cat image.
+(define cat .)
+
+; Sum of width and height of image.
+(+ (image-width cat) (image-height cat))
